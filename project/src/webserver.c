45864de72b7f089fb9db55e1eeeab500d2a3a9cb
Peter <keifa.peter@gmail.com>
2017-09-26 15:37:30 +0200

    Started with multitasking

--- project/src/webserver.c
+++ project/src/webserver.c
@@ -90,7 +90,7 @@ void setUp(int *sd, const int port, struct sockaddr_in *sin) { // Function to se
 	}
 }
 
-void acceptAndRecData(int *sd, int *sd_current, struct sockaddr_in *pin, int *addrlen, char buf[BUFSIZE]) { // Function to accept and recieve data
+int acceptAndRecData(int *sd, int *sd_current, struct sockaddr_in *pin, int *addrlen, char buf[BUFSIZE]) { // Function to accept and recieve data
 	// Wait for client to send us an request
 	// Get new socket file descriptor to use for this single connection
 	// pin = space where information about where the connection is comming from
@@ -113,6 +113,8 @@ void acceptAndRecData(int *sd, int *sd_current, struct sockaddr_in *pin, int *ad
 	// Show info about the client
 	printf("Request from %s:%i\n", inet_ntoa((*pin).sin_addr), ntohs((*pin).sin_port));
 	printf("Request: %s", buf);
+
+	return 1;
 }
 
 // Return NULL if there was an error else return current date as a string
@@ -149,11 +151,11 @@ char* getFileLastModified(char *path) { // Function to return last date and time
 	return response;
 }
 
-char* handleGet(char* file) { // Function to handle GET command.
+char* handleRequest(char *requestType, char* file) { // Function to handle GET command.
 	
 	// Variables used in the response
 	char *protocol = "HTTP/1.0 ";
-	char *responseCode = "200 OK\n";
+	char *responseCode = "400 Bad Request\n";
 	char *time = getDate();
 	char *server = "Server: runs something\n"; //!!!!
 	char *lastModified = "TEMP"; //!!!!
@@ -173,6 +175,8 @@ char* handleGet(char* file) { // Function to handle GET command.
 	char *pathToRequestedFile;
 	char *fileName = calloc(strlen(file), sizeof(char));
 	
+	
+	
 	realpath(file, fileName); // Check for .. or .
 
 	if(strcmp(fileName, "/")==0)
@@ -180,18 +184,25 @@ char* handleGet(char* file) { // Function to handle GET command.
 	else if(fileName[0] = '/')
 		fileName++;
 
-
+	
 	pathToRequestedFile = calloc(strlen(pathToWWW) + strlen(fileName) + 1, sizeof(char)); // Make space in memory for file path string
 	strcpy(pathToRequestedFile, pathToWWW); // Set two char arrays into one, filepath+filename
 	strcat(pathToRequestedFile, fileName);
 	
-	printf("open file in path: %s\n", pathToRequestedFile);
-	fp = fopen(pathToRequestedFile, "r"); // Open file from recieved path and filename string
+	if(strcmp(requestType, "HEAD") == 0 || strcmp(requestType, "GET") == 0) {
+		printf("open file in path: %s\n", pathToRequestedFile);
+		fp = fopen(pathToRequestedFile, "r"); // Open file from recieved path and filename string
+		
+		responseCode = "200 OK\n";
 	
-	if(fp == NULL) { 
-		fp = fopen("../www/error404.html", "r");	
-		responseCode = "404 Not Found\n";
+		if(fp == NULL) { 
+			fp = fopen("../www/error404.html", "r");	
+			responseCode = "404 Not Found\n";
+		}
 	}
+	else {
+		fp = fopen("../www/error400.html", "r");
+	}	
 
 	if(fp == NULL) { 
 		fp = fopen("../www/error500.html", "r");	
@@ -199,6 +210,7 @@ char* handleGet(char* file) { // Function to handle GET command.
 	}
 
 	if(fp != NULL) {
+		
 		fseek(fp, 0, SEEK_END);
 		fileSize = ftell(fp); // Set filesize to size of opened file
 		
@@ -215,11 +227,14 @@ char* handleGet(char* file) { // Function to handle GET command.
 		responseCode = "500 Internal Server Error\n";
 	}
 
+	if(strcmp(requestType, "HEAD") == 0) {
+		
+	}	
+
 	lastModified = getFileLastModified(pathToRequestedFile); // Set value for last modified with function
+	
 	int pageSize = strlen(page);
 	contentLength = calloc(strlen(contentLengthStart) + sizeof(pageSize) + 1, sizeof(char));
-	//strcpy(contentLength, contentLengthStart);
-	//strcat(contentLength, 
 	sprintf(contentLength, contentLengthStart, pageSize);
 
 	response = calloc(	strlen(protocol) +
@@ -254,40 +269,15 @@ char* handleGet(char* file) { // Function to handle GET command.
 }
 
 char* handleBuf(char buf[BUFSIZE]) { // Function to handle recieved data
-	// Variables used 
-	char *token;
 	
-	//char *temp = strdup(buf);
 	char *temp = calloc(strlen(buf) + 1, sizeof(char));
 	strcpy(temp, buf);
 	
 	const char *delim = " \0";
-	token = strtok(temp, delim);
-	
-	char *response;
-	
-	if(token != NULL && *token != '\r') {
-		// WIP
-		// Implement function handling head and return response code
-		if(strcmp(token, "HEAD") == 0) {
-			response = "HEAD\n"; 
-		}
-		// WIP
-		// Implement function handling get and return response code
-		else if(strcmp(token, "GET") == 0) {
-			token = strtok(NULL, delim); // Set token to next part of char array, filename
-			//token[strlen(token) - 2] = '\0'; // Remove junk from char array
-			
-			response = handleGet(token); // Call handleget and set the response to var response
-		}
-		else {
-			response = "ERROR 501\n"; // If command not implemented, return error 501
-		}
-	}
-	else{
-		response = "ERROR 400\n"; // If something else went wrong, return error 400
-	}
-	
+	char *requestType = strtok(temp, delim);	
+	char *fileName = strtok(NULL, delim);
+	char *response = handleRequest(requestType, fileName);
+
 	printf("Response message:\n%s\n", response);
 
 	return response;
@@ -301,33 +291,52 @@ int main(int argc, char* argv[]) {
 	int addrlen;
 	char buf[BUFSIZE];
 	char *response = "No response";
-	
+	int pid = getpid();	
+
 	parameterHandling(argc, &port, argv); // Call function to handle parameters entered when starting the webserver
 
 	printf("Listening to port: %d\n", port); // Showing what port has been set to listen to
 	
 	setUp(&sd, port, &sin);	// Set up the socket to listen for requests
 	
+	for(int i = 0; i < BUFSIZE; i++) { // Remove junk from buffer
+		buf[i] = '\0';
+	}
+
 	// Figure out where to fork what, suggestion, fork in start of while
-	while(1) {
-		for(int i = 0; i < BUFSIZE; i++) { // Remove junk from buffer
-			buf[i] = '\0';
-		}				
-		acceptAndRecData(&sd, &sd_current, &pin, &addrlen, buf); // Call function to handle recieved data
-		response = handleBuf(buf); // Call function to handle a response
+	while(pid != 0) {
+					
+		if(acceptAndRecData(&sd, &sd_current, &pin, &addrlen, buf)) {
+			pid = fork();
+		}
+
+		//response = handleBuf(buf); // Call function to handle a response
 	
 		// Send a response to the client
-		if(send(sd_current, response, strlen(response) + 1, 0) == -1) { // Send a response to client, if it does not work, show error.
-			perror("send");
-			exit(-1);
-		}
+		//if(send(sd_current, response, strlen(response) + 1, 0) == -1) { // Send a response to client, if it does not work, show error.
+		//	perror("send");
+		//	exit(-1);
+		//}
 	
 		// Close current socket
-		close(sd_current); // Close current socket
+		//close(sd_current); // Close current socket
+		
 	}	
 	
-	// Close socket
-	close(sd);
+	response = handleBuf(buf); // Call function to handle a response
 	
-	return 0;
+	// Send a response to the client
+	if(send(sd_current, response, strlen(response) + 1, 0) == -1) { // Send a response to client, if it does not work, show error.
+		perror("send");
+		exit(-1);
+	}
+	
+	// Close current socket
+	close(sd_current); // Close current socket
+
+
+	// Close socket
+	//close(sd);
+	
+	exit(0);
 }
